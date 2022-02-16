89f1fe8e84925b107fc29c42e433962a481bf393
Mikolaj Figurski <meek.f80@gmail.com>
2022-02-16 18:42:29 -0500

    Read csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate csv;
+#[macro_use]
+extern crate serde_derive;
+
+use std::error::Error;
+use std::io;
+use std::process;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    text_keywords: String,
+}
+
+fn read_csv(filename: &str) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_path(filename)?;
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = read_csv("data/data.csv") {
+        println!("error reading csv {}", err);
+        process::exit(1);
+    }
 }
