10e1c48c5c49cab58b8c9b1adc153a80a8d2a142
Mikolaj Figurski <meek.f80@gmail.com>
2022-02-16 19:12:53 -0500

    Parse arguments

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate csv;
 #[macro_use]
 extern crate serde_derive;
 
+use std::env;
 use std::error::Error;
-use std::io;
 use std::process;
 
 #[derive(Debug, Deserialize)]
@@ -21,8 +21,22 @@ fn read_csv(filename: &str) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    if let Err(err) = read_csv("data/data.csv") {
-        println!("error reading csv {}", err);
+    let args: Vec<String> = env::args().collect();
+    // print out all args after first arg
+    if args.len() != 4 {
+        println!(
+            "Usage: {} <csv_file> <support_threshold> <output_file>",
+            args[0]
+        );
+        process::exit(1);
+    }
+    let filename = &args[1];
+    let threshold: u32 = args[2].parse().unwrap();
+    let output = &args[3];
+    println!("All above {}: '{}' -> '{}' ", threshold, filename, output);
+
+    if let Err(err) = read_csv(filename) {
+        println!("error reading csv '{}': {}", filename, err);
         process::exit(1);
     }
 }
