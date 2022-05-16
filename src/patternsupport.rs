78542146607f95e6b25ebebeb572fcbbd7568d5d
Mikolaj Figurski <meek.f80@gmail.com>
2022-05-16 15:03:06 -0400

    Add patternsupport test

--- src/patternsupport.rs
+++ src/patternsupport.rs
@@ -7,7 +7,6 @@ use crate::read;
 use hashbrown::HashMap;
 use intersect::IntersectSorted;
 use rayon::prelude::*;
-use std::sync::{Arc, Mutex};
 
 /// Keyword List that stores string at their encodings. Shortens string pointers to u32
 pub type Keywords = Vec<String>;
@@ -17,26 +16,16 @@ type SupportingRowIds = Vec<u32>;
 pub type PatternSupport = HashMap<KeywordIds, SupportingRowIds>;
 
 pub trait RecursivelyBuildable {
-    fn build_next(
-        &self,
-        keyword_list: &Keywords,
-        k1_support: &PatternSupport,
-        min_support: usize,
-    ) -> PatternSupport;
+    fn build_next(&self, k1_support: &PatternSupport, min_support: usize) -> PatternSupport;
 }
 
 impl RecursivelyBuildable for PatternSupport {
-    fn build_next(
-        &self,
-        keyword_list: &Keywords,
-        k1_support: &PatternSupport,
-        min_support: usize,
-    ) -> PatternSupport {
+    fn build_next(&self, k1_support: &PatternSupport, min_support: usize) -> PatternSupport {
         PatternSupport::from_par_iter(
             self.par_iter()
                 .map(|(prev_pat, prev_sup)| {
                     let last = *prev_pat.last().unwrap();
-                    (last + 1..keyword_list.len() as u32)
+                    (last + 1..k1_support.len() as u32)
                         .map(|i| {
                             let mut new_pat = prev_pat.clone();
                             new_pat.push(i);
@@ -64,4 +53,26 @@ pub fn parse_keyword_support(base: read::KeywordSupport) -> (Keywords, PatternSu
         pattern.insert(vec![new_id as u32], sup.to_vec());
     }
     (keywords, pattern)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::patternsupport::*;
+
+    #[test]
+    fn it_correctly_builds_next() {
+        let mut k1 = PatternSupport::new();
+        k1.insert_unique_unchecked(vec![0], vec![0, 1, 2, 3, 4, 5]);
+        k1.insert_unique_unchecked(vec![1], vec![1, 2, 3, 4, 5]);
+        k1.insert_unique_unchecked(vec![2], vec![2, 3, 4, 5]);
+        k1.insert_unique_unchecked(vec![3], vec![3, 4, 5]);
+        let min_sup = 3;
+        let k2 = k1.build_next(&k1, min_sup);
+        assert_ne!(k1, k2);
+        assert_eq!(k2.len(), 6);
+        for (pat, sup) in k2.into_iter() {
+            assert_eq!(pat.len(), 2);
+            assert!(sup.len() >= min_sup);
+        }
+    }
 }
